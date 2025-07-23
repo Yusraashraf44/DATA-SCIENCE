bc93fb2624a6ddb0777376f42f941c0d77c1db85
YUSRA <yusraash44@gmail.com>
2025-07-23 11:34:01 +0530

    Add files via upload

--- WEEK-2/MatrixTransformation3D.py
+++ WEEK-2/MatrixTransformation3D.py
@@ -5,6 +5,7 @@ def translationMatrix(tx=0, ty=0, tz=0):
                          [0,0,1,tz],
                          [0,0,0,1 ]])
 matrix=translationMatrix(1,1,1)
+print("Matrix Translation:")
 print(matrix)
 
 def rotationMatrix(degree):
@@ -15,6 +16,7 @@ def rotationMatrix(degree):
                          [0,s,c,0],
                          [0,0,0,1]])
 matrix=rotationMatrix(30)
+print("Matrix Rotation about x axis:")
 print(matrix)
 
 def rotationMatrix(degree):
@@ -25,6 +27,7 @@ def rotationMatrix(degree):
                          [-s,0,c,0],
                          [0,0,0,1]])
 matrix=rotationMatrix(30)
+print("matrix rotation about y axis:")
 print(matrix)
 
 def rotationMatrix(degree):
@@ -35,4 +38,5 @@ def rotationMatrix(degree):
                          [0,0,1,0],
                          [0,0,0,1]])
 matrix=rotationMatrix(30)
+print("matrix rotation about z axis:")
 print(matrix)
